a4dc685fa34120cbf5e595d112c1d1ca10551400
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-06-21 16:24:55 +0100

    added products page template

---
+++ src/templates/products-page.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const productsPage = props => {
+  return (
+    <div>
+      <h1>This is products page</h1>
+    </div>
+  )
+}
+
+productsPage.propTypes = {}
+
+export default productsPage
